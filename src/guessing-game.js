6505dc6759c8d2eb70210c872c525b1e25edb595
dbataeva <b.daffa.v@yandex.ru>
2022-08-05 19:57:29 +0300

    feat: game works

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
+    _min;
+    _max;
+
+    _lastNumber;
+
     constructor() {}
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        this._lastNumber = Math.round((this._max + this._min) / 2);
+        return this._lastNumber;
     }
 
     lower() {
-
+        this._max = this._lastNumber;
     }
 
     greater() {
-
+        this._min = this._lastNumber;
     }
 }
 
